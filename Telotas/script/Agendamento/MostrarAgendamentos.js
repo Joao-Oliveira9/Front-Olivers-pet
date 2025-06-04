c29d104a4571b1608a8c52a6588adf692292b47e
Igor <igormatosgaeta@gmail.com>
2025-06-04 00:39:46 -0300

    concluir agendamento

--- Telotas/script/Agendamento/MostrarAgendamentos.js
+++ Telotas/script/Agendamento/MostrarAgendamentos.js
@@ -1,4 +1,33 @@
+function concluirAgendamento(id) {
+
+        concluir = {
+            id: id
+        }
+        fetch('http://localhost:8080/concluirAgendamento', {
+            method: 'PATCH',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(concluir)
+        })
+        .then(response => {
+            if(!response.ok) throw new Error('');
+            
+            return response.json();
+        })
+        .then(data => {
+            if(data.status !== "OK") {
+                alert("Erro ao concluir agendamento.");
+                return;
+            }
+
+            alert('✅ ' + data.message);
+            window.location.href = "../../pages/telaAgenda/index.html"; 
+        })
+    }
+
 document.addEventListener('DOMContentLoaded', () => {
+    
     fetch("http://localhost:8080/mostrarAgendamentos",{method:'GET'})
         .then(response => {
             if(!response.ok) throw new Error('Erro ao buscar os agendamentos');
@@ -24,6 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     <td>${p.telefone_do_dono}</td>
                     <td>${p.status}</td>
                     <td><span class="edit-icon" onClick="window.location.href='../telaEditarHorario/index.html?codigo=${p.id}&data=${p.data}'">✏️</span></td>
+                    <td><span class="concluir-icon" onclick='concluirAgendamento("${p.id}")'>✅</span></td>
                 </tr>`;
                 tbody.innerHTML += row;
             });
